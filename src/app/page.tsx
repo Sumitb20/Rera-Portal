c808c5af76b4edd2bb7039c375059fa9002d0bb4
Build Tek <sumitbhoyar2000@gmail.com>
2026-04-19 23:16:06 +0530

    Replace RootPage with AboutPage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
-import { redirect } from 'next/navigation';
+import React from 'react';
+import Navbar from '@/components/Navbar';
+import AboutHero from './components/AboutHero';
+import HowItWorks from './components/HowItWorks';
+import TeamSection from './components/TeamSection';
+import ReraCommitment from './components/ReraCommitment';
+import AiFeatureAbout from './components/AiFeatureAbout';
+import AboutFooter from './components/AboutFooter';
 
-export default function RootPage() {
-  redirect('/home-landing-page');
+export default function AboutPage() {
+  return (
+    <div className="min-h-screen bg-[#0B1120]">
+      <Navbar />
+      <div className="pt-16">
+        <AboutHero />
+        <HowItWorks />
+        <TeamSection />
+        <ReraCommitment />
+        <AiFeatureAbout />
+        <AboutFooter />
+      </div>
+    </div>
+  );
 }
